7efecee7f916ed354bc032753ee6a0644a78976b
Alban Dericbourg <a.dericbourg@criteo.com>
2017-04-06 13:58:01 +0200

    Add lists

--- src/toto.erl
+++ src/toto.erl
@@ -2,9 +2,14 @@
 
 %% API
 % /2 : function's arity (arguments count)
--export([add/2, divide/2]).
+-export([add/1, add/2, divide/2]).
 
+add([]) -> 0;
+add([Head | Tail]) -> Head + add(Tail).
+
+add({Xa, Ya}, {Xb, Yb}) -> {Xa + Ya, Xb + Yb};
 add(A, B) -> A + B.
 
-divide(A,0) -> infinite;
+divide(A, 0) when A >= 0 -> p_infinite;
+divide(A, 0) when A < 0 -> m_infinite;
 divide(A, B) -> A / B.
